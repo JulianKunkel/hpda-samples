52fe36b956ab33c14dcd67b90bdea61d3430ee5a
Julian M. Kunkel <juliankunkel@googlemail.com>
2021-10-24 11:14:32 +0200

    Nai

--- install/hadoop.sh
+++ install/hadoop.sh
@@ -4,3 +4,61 @@ VERSION=hadoop-3.3.1
 wget https://downloads.apache.org/hadoop/common/$VERSION/$VERSION.tar.gz 
 
 tar -xf $VERSION*gz
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" > /etc/environment
+
+ln -s $PWD/$VERSION hadoop
+
+echo Configuring Hadoop
+mkdir datanode
+mkdir namenode
+# See: https://tecadmin.net/install-hadoop-on-ubuntu-20-04/
+
+cat >> hadoop/etc/hadoop/core-site.xml << EOF
+<configuration>
+        <property>
+                <name>fs.defaultFS</name>
+                <value>hdfs://localhost:9000</value>
+        </property>
+</configuration>
+EOF
+
+cat >> hadoop/etc/hadoop/hdfs-site.xml << EOF
+<configuration>
+        <property>
+                <name>dfs.replication</name>
+                <value>1</value>
+        </property>
+ 
+        <property>
+                <name>dfs.name.dir</name>
+                <value>file:///DIR/namenode</value>
+        </property>
+ 
+        <property>
+                <name>dfs.data.dir</name>
+                <value>file:///DIR/datanode</value>
+        </property>
+</configuration>
+EOF
+sed -i "s#DIR#$PWD#" hadoop/etc/hadoop/hdfs-site.xml
+
+cat >> hadoop/etc/hadoop/mapred-site.xml << EOF
+<configuration>
+        <property>
+                <name>mapreduce.framework.name</name>
+                <value>yarn</value>
+        </property>
+</configuration>
+EOF
+
+cat >> hadoop/etc/hadoop/yarn-site.xml << EOF
+<configuration>
+        <property>
+                <name>yarn.nodemanager.aux-services</name>
+                <value>mapreduce_shuffle</value>
+        </property>
+</configuration>
+EOF
+
+hdfs namenode -format 
