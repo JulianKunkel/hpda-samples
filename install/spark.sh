ca1d60c1d374b89da6c635a651be5864e05ef57f
Julian M. Kunkel <juliankunkel@googlemail.com>
2021-12-05 19:56:05 +0100

    Spark: Updated install instructions

--- install/spark.sh
+++ install/spark.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-pip3 install pyspark[pandas_on_spark,sql] plotly
+sudo apt-get install jupyter-notebook
+sudo pip3 install pyspark[pandas_on_spark,sql] chart-studio
+
+#ipython3 profile create pyspark
+# Create startup file for Spark
+#mkdir startup
+#cp pyspark_setup.py startup
+#chmod 755 startup/*
+
+echo now run:
+echo jupyter notebook 
+
